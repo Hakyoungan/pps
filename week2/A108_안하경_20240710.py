fa02e8a78ea6d3f6502018ad60f63ec7b911e14a
Hakyoungan <hakyoung8461@gmail.com>
2024-07-14 22:14:00 +0900

    pps exercise 25

--- week2/A108_안하경_20240710.py
+++ week2/A108_안하경_20240710.py
@@ -1,27 +1,25 @@
-def transpose(matrix):
-    # Check if the matrix is empty
-    if not matrix or not matrix[0]:
-        return []
-    
-    # Number of rows and columns in the input matrix
-    rows, cols = len(matrix), len(matrix[0])
-    
-    # Create the transposed matrix with dimensions (cols, rows)
-    transposed_matrix = [[0] * rows for _ in range(cols)]
-    
-    # Fill the transposed matrix with appropriate values
-    for i in range(rows):
-        for j in range(cols):
-            transposed_matrix[j][i] = matrix[i][j]
-    
-    return transposed_matrix
+#수 뒤집기
+#원래의 수와 뒤집은 수를 더한 것이 팰린드롬인지 판별
+#수 124를 뒤집으면 421이 되고 이 두 수를 합하면 545가 된다. 124와 같이 원래 수와 뒤집은 수를 합한 수가 좌우 대칭이 되는지 테스트 하는 프로그램을 작성하시오.
 
-# Example 1
-matrix1 = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
-print("Input:", matrix1)
-print("Output:", transpose(matrix1))
+def isPalindrome(num):
+    return str(num) == str(num)[::-1]
 
-# Example 2
-matrix2 = [[1, 2, 3], [4, 5, 6]]
-print("Input:", matrix2)
-print("Output:", transpose(matrix2))
+def reverseAndAdd(N):
+    reversed_N = int(str(N)[::-1])
+    return N + reversed_N
+
+def tryout():
+    T = int(input())
+    results = []
+    for _ in range(T):
+        N = int(input())
+        result = reverseAndAdd(N)
+        if isPalindrome(result):
+            results.append("YES")
+        else:
+            results.append("NO")
+    
+    for result in results:
+        print(result)
+tryout()
